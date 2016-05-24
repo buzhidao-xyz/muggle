07d23f10b02c55aafba22e40bfdb57d6771ae6b6
buzhidao <imbzd.wang@aliyun.com>
2016-05-24 14:44:47 +0800

    base.js 调整

--- app/scripts/controllers/base.js
+++ app/scripts/controllers/base.js
@@ -22,15 +22,23 @@ angular.module('muggleApp')
       'Karma'
     ];
 
+    //预定义变量
+    $scope.errormsg = "网络错误 请求失败！";
+
+    //页面msg提示
+    $scope.alertShow = function (msg, flag) {
+      alert(msg);
+    }
+
     //监听事件 - apiRequest.failed
 		$scope.$on('apiRequest.failed', function (event, d){
-			var msg = '网络错误 请求失败！';
-			alert(msg);
+			$scope.alertShow($scope.errormsg);
 		});
 
     //写入cookie-userinfo
     $scope.gsuserinfo = function ($userinfo) {
-      if ($userinfo && ("id" in $userinfo) && $userinfo.id) {
+      if ($userinfo && ("sessionid" in $userinfo) && $userinfo.sessionid) {
+        $cookies.putObject('sessionid', $userinfo.sessionid);
         $cookies.putObject('userinfo', $userinfo);
       }
 
@@ -56,10 +64,19 @@ angular.module('muggleApp')
 
     //统一API返回数据处理方法
     $scope.apiResult = function (data, params) {
-      return data.data;
+      //如果status==0 成功
+      if (data.status==0) {
+        return data.data;
+      } else if (data.status==-4) {
+        //未登录用户
+        $location.path('/login');
+      } else {
+        $scope.alertShow($scope.errormsg);
+      }
     }
 
     return {
+      alertShow: $scope.alertShow,
       gsuserinfo: $scope.gsuserinfo,
       ususerinfo: $scope.ususerinfo,
       apiResult: $scope.apiResult
