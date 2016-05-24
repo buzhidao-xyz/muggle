a23f79949980edbb251407fc413948d8bef129d6
buzhidao <imbzd.wang@aliyun.com>
2016-05-24 17:11:23 +0800

    登录控制逻辑添加

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,23 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
+  .factory('UserInterceptor', ["$q","$rootScope",function ($q, $rootScope) {
+    return {
+      request: function (config) {
+        return config;
+      },
+      response: function (response) {
+        return response;
+      },
+      requestError: function (rejection) {
+        return $q.reject(rejection);
+      },
+      responseError: function (rejection) {
+        return $q.reject(rejection);
+      }
+    }
+  }])
+  .config(function ($routeProvider, $httpProvider) {
     $routeProvider
       .when('/main', {
         templateUrl: 'views/main.html',
@@ -44,6 +60,8 @@ angular
       .otherwise({
         redirectTo: '/login'
       });
+
+    $httpProvider.interceptors.push('UserInterceptor');
   }).run(['$rootScope', '$window', '$location', '$log', function ($rootScope, $window, $location, $log) {
     //监听事件 - 路由切换开始
     $rootScope.$on('$routeChangeStart', function (){
