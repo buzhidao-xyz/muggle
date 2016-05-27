504ef5050cfeb8a0726096818c80fef0237da175
buzhidao <imbzd.wang@aliyun.com>
2016-05-27 18:58:08 +0800

    课程详情页

--- app/scripts/controllers/courseview.js
+++ app/scripts/controllers/courseview.js
@@ -25,40 +25,68 @@ angular.module('muggleApp')
       //BaseCtrl
       var BaseCtrl = $controller('BaseCtrl', {$rootScope: $rootScope, $scope: $scope});
 
+      //courseid
+      var courseid = $location.search().courseid;
+      //chapterid
+      var chapterid = $location.search().chapterid;
+
+      //课程-节信息
+      $scope.chapterinfo = {}
+
       //获取课程详情
       $scope.doCourseView = function () {
-        //courseid
-        var courseid = $location.search().courseid;
-
         var data = {
-          courseid: courseid
+          id: courseid
         }
         var data = BaseCtrl.apiRequestData(data);
         //getcourseview
         $CourseService.getcourseview({}, data);
 
-        //监听-coursesview.success
-        $scope.$on('coursesview.success', function (event, d) {
+        //监听 - getcourseview.success
+        $scope.$on('getcourseview.success', function (event, d) {
           $scope.$courseview = BaseCtrl.apiResult($CourseService.courseview);
+
+          //遍历课程的章节信息
+          var ai = 1;
+          for (var k in $scope.$courseview.chapterIs) {
+            var chapterIid = $scope.$courseview.chapterIs[k].chapterid;
+            var lll = $scope.$courseview.chapterIIs[chapterIid].length;
+            $scope.$courseview.chapterIs[k].jienum = lll;
+
+            $scope.$courseview.chapterIs[k].expandclass = "";
+            if (ai==1) $scope.$courseview.chapterIs[k].expandclass = "expand";
+
+            //遍历节信息
+            var i = 1;
+            for (var j in $scope.$courseview.chapterIIs[chapterIid]) {
+              if (ai==1 && i==1) $scope.chapterinfo = $scope.$courseview.chapterIIs[chapterIid][j];
+
+              var classn = "";
+              if (i==1) classn = "first";
+              if (i==lll) classn = "last";
+              if (lll==1) classn = "first last";
+              $scope.$courseview.chapterIIs[chapterIid][j].classn = classn;
+              i++;
+            }
+
+            ai++;
+          }
+
+          //获取课程-节信息
+          $scope.chapterid = $scope.chapterinfo.chapterid;
+          console.log($scope.chapterid);
         })
       }
       $scope.doCourseView();
 
-      //播放课程
-      $scope.viewClass = function () {
-        function polyv() {
-          if (typeof polyvObject=='undefined') {
-            setTimeout("polyv()", 10);
-          } else {
-            var player = polyvObject('#plv_93d816c8ccec0d19784cf4f277d9b4fa_9').videoPlayer({
-                'width':'815',
-                'height':'458',
-                'vid' : '93d816c8ccec0d19784cf4f277d9b4fa_9'
-            });
-          }
-        }
-        polyv();
+      //视频播放
+      $scope.videoViewClass = function () {
+        var player = polyvObject('#videoview').videoPlayer({
+          'width':'815',
+          'height':'458',
+          'vid' : '93d816c8ccec0d19784cf4f277d9b4fa_9'
+        });
       }
-      $scope.viewClass();
+      $scope.videoViewClass();
 
     }]);
