a377d636cab77e5b6ace512d74f13f3a6339391c
yanghui <yanghui@imooly.com>
2016-06-01 15:17:11 +0800

    打包文件修改。

--- Gruntfile.js
+++ Gruntfile.js
@@ -412,8 +412,9 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '*.html',
-            'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'images/**',
+            'styles/fonts/{,*/}*.*',
+            'plugins/{,*/}*.min.js'
           ]
         }, {
           expand: true,
